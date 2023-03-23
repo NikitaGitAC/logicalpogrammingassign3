504de695a1585b2598997ef43c11d10b9118b304
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-23 18:02:08 +0530

    finding programming op

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using logical_programming_problems;
+
+namespace logicalprogram
+{
+    class program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("WELCOME to logical programming problem");
+            //Fibonacciseries.callvalue();
+            //PerfectNumber.FindPerfectNumber();
+            //PrimeNumber.findprimenumberornot();
+            //ReverseNumber.findreversenum();
+            //StopWatch.displaystopwatchtime();
+            Couponnumber.FindCouponNumber();
+        }
+    }
+}
