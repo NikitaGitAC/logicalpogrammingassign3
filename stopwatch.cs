2e3fa4e740933bec00f4f462d609272fed927b0e
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-23 18:17:35 +0530

    geeting op of stopwatch

---
+++ stopwatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_programming_problems
+{
+    internal class StopWatch
+    {
+        public static void displaystopwatchtime()
+        {
+            Console.WriteLine("Press any key to start the time");
+            Console.ReadKey();
+            Console.WriteLine(" ");
+            DateTime starttime = DateTime.Now;
+
+            Console.WriteLine("Press any key to stop the time");
+            Console.ReadKey();
+            Console.WriteLine(" ");
+            DateTime stoptime = DateTime.Now;
+
+            TimeSpan elapsed = starttime - stoptime;
+            Console.WriteLine("Elapsed time " + elapsed);
+        }
+    }
+}
