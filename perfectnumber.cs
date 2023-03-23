5743a5a16116373f2cdf59fe7f2b4abfb1c5cf6c
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-23 18:08:33 +0530

    geeting op of perfectnum

---
+++ perfectnumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_programming_problems
+{
+    public class PerfectNumber
+    {
+        public static void FindPerfectNumber()
+        {
+            int sum = 0;
+
+
+            Console.WriteLine("Find the perfect number");
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                    Console.Write($" {i} + ");
+                }
+            }
+
+            if (num == sum)
+            {
+                Console.WriteLine($"Given {num} is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine($"Given {num} is not a perfect number");
+            }
+        }
+    }
+}
